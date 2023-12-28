57fe4e4fcf1a04e4eb6e255e27aad6cabee1ee97
brownnicholasj <brown.nicholasj@gmail.com>
2023-12-28 08:42:04 -0600

    finish styling for now

--- src/components/leaderboard/Leaderboard.js
+++ src/components/leaderboard/Leaderboard.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { API_URL } from '../../constants';
+// import { API_URL } from '../../constants';
 import TableContainer from '../table/TableContainer';
 import { GETLEADERBOARDDATA } from '../../helpers/backEndData';
 import UserTeamsTable from './UserTeamsTable';
